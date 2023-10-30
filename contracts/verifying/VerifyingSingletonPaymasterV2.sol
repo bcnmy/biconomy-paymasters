c0b7a68c5b275d52b44f34ef89f147736cc82248
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2023-10-31 02:31:00 +0530

    contract with positive tests

--- contracts/verifying/VerifyingSingletonPaymasterV2.sol
+++ contracts/verifying/VerifyingSingletonPaymasterV2.sol
@@ -1,9 +1,386 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.17;
 
-// todo Add pricing
-// Instead of paymaster helpers write parsePaymasterData methods
+/* solhint-disable reason-string */
+/* solhint-disable no-inline-assembly */
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {UserOperation, UserOperationLib} from "@account-abstraction/contracts/interfaces/UserOperation.sol";
+import "../BasePaymaster.sol";
+import {VerifyingPaymasterErrors} from "../common/Errors.sol";
 
-contract VerifyingSingletonPaymasterV2 {
+contract VerifyingSingletonPaymasterV2 is
+    BasePaymaster,
+    ReentrancyGuard,
+    VerifyingPaymasterErrors
+{
+    using ECDSA for bytes32;
+    using UserOperationLib for UserOperation;
 
+    uint32 private constant PRICE_DENOMINATOR = 1e6;
+
+    // Review if we only wanna encode until signature and have signature offset
+    // paymasterAndData: concat of [paymasterAddress(address), abi.encode(paymasterId, validUntil, validAfter, priceMarkup): makes up 32*4 bytes, signature]
+    uint256 private constant VALID_PND_OFFSET = 20;
+
+    uint256 private constant SIGNATURE_OFFSET = 148;
+
+    // Gas used in EntryPoint._handlePostOp() method (including this#postOp() call)
+    uint256 private unaccountedEPGasOverhead;
+    mapping(address => uint256) public paymasterIdBalances;
+
+    address public verifyingSigner;
+
+    uint32 public fixedPriceMarkup; // immutable? constant? 1.1e6
+
+    event EPGasOverheadChanged(
+        uint256 indexed _oldValue,
+        uint256 indexed _newValue
+    );
+
+    event FixedPriceMarkupChanged(
+        uint32 indexed _oldValue,
+        uint32 indexed _newValue
+    );
+
+    event VerifyingSignerChanged(
+        address indexed _oldSigner,
+        address indexed _newSigner,
+        address indexed _actor
+    );
+    event GasDeposited(address indexed _paymasterId, uint256 indexed _value);
+    event GasWithdrawn(
+        address indexed _paymasterId,
+        address indexed _to,
+        uint256 indexed _value
+    );
+    event GasBalanceDeducted(
+        address indexed _paymasterId,
+        uint256 indexed _charge
+    );
+    event FeeCollected(uint256 indexed _premium);
+
+    // Review if fixed markup is needed in constructor (can init with value or make constant)
+    constructor(
+        address _owner,
+        IEntryPoint _entryPoint,
+        address _verifyingSigner,
+        uint32 _fixedPriceMarkup
+    ) payable BasePaymaster(_owner, _entryPoint) {
+        if (address(_entryPoint) == address(0)) revert EntryPointCannotBeZero();
+        // Review // maybe <= 1300000
+        require(_fixedPriceMarkup <= PRICE_DENOMINATOR * 2, "markup too high");
+        if (_verifyingSigner == address(0))
+            revert VerifyingSignerCannotBeZero();
+        assembly {
+            sstore(verifyingSigner.slot, _verifyingSigner)
+        }
+        unaccountedEPGasOverhead = 12000;
+        fixedPriceMarkup = _fixedPriceMarkup;
+    }
+
+    /**
+     * @dev Add a deposit for this paymaster and given paymasterId (Dapp Depositor address), used for paying for transaction fees
+     * @param paymasterId dapp identifier for which deposit is being made
+     */
+    function depositFor(address paymasterId) external payable nonReentrant {
+        if (paymasterId == address(0)) revert PaymasterIdCannotBeZero();
+        if (msg.value == 0) revert DepositCanNotBeZero();
+        paymasterIdBalances[paymasterId] =
+            paymasterIdBalances[paymasterId] +
+            msg.value;
+        entryPoint.depositTo{value: msg.value}(address(this));
+        emit GasDeposited(paymasterId, msg.value);
+    }
+
+    /**
+     * @dev get the current deposit for paymasterId (Dapp Depositor address)
+     * @param paymasterId dapp identifier
+     */
+    function getBalance(
+        address paymasterId
+    ) external view returns (uint256 balance) {
+        balance = paymasterIdBalances[paymasterId];
+    }
+
+    /**
+     @dev Override the default implementation.
+     */
+    function deposit() public payable virtual override {
+        revert("user DepositFor instead");
+    }
+
+    /**
+     * @dev Withdraws the specified amount of gas tokens from the paymaster's balance and transfers them to the specified address.
+     * @param withdrawAddress The address to which the gas tokens should be transferred.
+     * @param amount The amount of gas tokens to withdraw.
+     */
+    function withdrawTo(
+        address payable withdrawAddress,
+        uint256 amount
+    ) public override nonReentrant {
+        if (withdrawAddress == address(0)) revert CanNotWithdrawToZeroAddress();
+        uint256 currentBalance = paymasterIdBalances[msg.sender];
+        if (amount > currentBalance)
+            revert InsufficientBalance(amount, currentBalance);
+        paymasterIdBalances[msg.sender] =
+            paymasterIdBalances[msg.sender] -
+            amount;
+        entryPoint.withdrawTo(withdrawAddress, amount);
+        emit GasWithdrawn(msg.sender, withdrawAddress, amount);
+    }
+
+    /**
+     * @dev Set a new verifying signer address.
+     * Can only be called by the owner of the contract.
+     * @param _newVerifyingSigner The new address to be set as the verifying signer.
+     * @notice If _newVerifyingSigner is set to zero address, it will revert with an error.
+     * After setting the new signer address, it will emit an event VerifyingSignerChanged.
+     */
+    function setSigner(address _newVerifyingSigner) external payable onlyOwner {
+        if (_newVerifyingSigner == address(0))
+            revert VerifyingSignerCannotBeZero();
+        address oldSigner = verifyingSigner;
+        assembly {
+            sstore(verifyingSigner.slot, _newVerifyingSigner)
+        }
+        emit VerifyingSignerChanged(oldSigner, _newVerifyingSigner, msg.sender);
+    }
+
+    function setUnaccountedEPGasOverhead(uint256 value) external onlyOwner {
+        uint256 oldValue = unaccountedEPGasOverhead;
+        unaccountedEPGasOverhead = value;
+        emit EPGasOverheadChanged(oldValue, value);
+    }
+
+    function setFixedPriceMarkup(uint32 _markup) external onlyOwner {
+        require(_markup <= PRICE_DENOMINATOR * 2, "markup too high");
+        uint32 oldValue = fixedPriceMarkup;
+        fixedPriceMarkup = _markup;
+        emit FixedPriceMarkupChanged(oldValue, _markup);
+    }
+
+    /**
+     * @dev This method is called by the off-chain service, to sign the request.
+     * It is called on-chain from the validatePaymasterUserOp, to validate the signature.
+     * @notice That this signature covers all fields of the UserOperation, except the "paymasterAndData",
+     * which will carry the signature itself.
+     * @return hash we're going to sign off-chain (and validate on-chain)
+     */
+    function getHash(
+        UserOperation calldata userOp,
+        address paymasterId,
+        uint48 validUntil,
+        uint48 validAfter,
+        uint32 priceMarkup
+    ) public view returns (bytes32) {
+        //can't use userOp.hash(), since it contains also the paymasterAndData itself.
+        return
+            keccak256(
+                abi.encode(
+                    userOp.getSender(),
+                    userOp.nonce,
+                    keccak256(userOp.initCode),
+                    keccak256(userOp.callData),
+                    userOp.callGasLimit,
+                    userOp.verificationGasLimit,
+                    userOp.preVerificationGas,
+                    userOp.maxFeePerGas,
+                    userOp.maxPriorityFeePerGas,
+                    block.chainid,
+                    address(this),
+                    paymasterId,
+                    validUntil,
+                    validAfter,
+                    priceMarkup
+                )
+            );
+    }
+
+    // Note: do not use this in validation phase
+    function getGasPrice(
+        uint256 maxFeePerGas,
+        uint256 maxPriorityFeePerGas
+    ) internal view returns (uint256) {
+        if (maxFeePerGas == maxPriorityFeePerGas) {
+            //legacy mode (for networks that don't support basefee opcode)
+            return maxFeePerGas;
+        }
+        return minuint256(maxFeePerGas, maxPriorityFeePerGas + block.basefee);
+    }
+
+    // Review below helpers
+    function minuint256(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+
+    function maxuint256(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a > b ? a : b;
+    }
+
+    function minuint32(uint32 a, uint32 b) internal pure returns (uint32) {
+        return a < b ? a : b;
+    }
+
+    function maxuint32(uint32 a, uint32 b) internal pure returns (uint32) {
+        return a > b ? a : b;
+    }
+
+    /**
+     * @dev Verify that an external signer signed the paymaster data of a user operation.
+     * The paymaster data is expected to be the paymaster and a signature over the entire request parameters.
+     * @param userOp The UserOperation struct that represents the current user operation.
+     * userOpHash The hash of the UserOperation struct.
+     * @param requiredPreFund The required amount of pre-funding for the paymaster.
+     * @return context A context string returned by the entry point after successful validation.
+     * @return validationData An integer returned by the entry point after successful validation.
+     */
+    // Review : could add dynamic price markup as part of paymasterAndData
+    // Then this min(dynamicMarkup, fixedPriceMarkup) could be used
+    function _validatePaymasterUserOp(
+        UserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 requiredPreFund
+    ) internal override returns (bytes memory context, uint256 validationData) {
+        // review: in this method try to resolve stack too deep (though via-ir is good enough)
+        (
+            address paymasterId,
+            uint48 validUntil,
+            uint48 validAfter,
+            uint32 priceMarkup,
+            bytes calldata signature
+        ) = parsePaymasterAndData(userOp.paymasterAndData);
+
+        bytes32 hash = getHash(
+            userOp,
+            paymasterId,
+            validUntil,
+            validAfter,
+            priceMarkup
+        );
+        context = "";
+        // Review: var can be removed
+        uint256 sigLength = signature.length;
+        // we only "require" it here so that the revert reason on invalid signature will be of "VerifyingPaymaster", and not "ECDSA"
+        if (sigLength != 65) revert InvalidPaymasterSignatureLength(sigLength);
+        //don't revert on signature failure: return SIG_VALIDATION_FAILED
+        if (
+            verifyingSigner != hash.toEthSignedMessageHash().recover(signature)
+        ) {
+            // empty context and sigFailed with time range provided
+            return (context, _packValidationData(true, validUntil, validAfter));
+        }
+
+        require(
+            priceMarkup <= 2e6,
+            "Verifying PM: price markup percentage too high"
+        );
+        // Review if below is needed (as paymaster service may pass 0 if dynamic pricing doesn't apply)
+        require(
+            priceMarkup >= 1e6,
+            "Verifying PM: price markup percentage too low"
+        );
+
+        // Review: may not be needed at all
+        address account = userOp.getSender();
+
+        // Review max or min
+        uint32 dynamicMarkup = maxuint32(priceMarkup, fixedPriceMarkup);
+
+        if (
+            (requiredPreFund * dynamicMarkup) / PRICE_DENOMINATOR >
+            paymasterIdBalances[paymasterId]
+        )
+            revert InsufficientBalance(
+                (requiredPreFund * dynamicMarkup) / PRICE_DENOMINATOR,
+                paymasterIdBalances[paymasterId]
+            );
+
+        context = abi.encode(
+            account,
+            paymasterId,
+            priceMarkup,
+            userOp.maxFeePerGas,
+            userOp.maxPriorityFeePerGas,
+            userOpHash
+        );
+
+        return (context, _packValidationData(false, validUntil, validAfter));
+    }
+
+    function parsePaymasterAndData(
+        bytes calldata paymasterAndData
+    )
+        public
+        pure
+        returns (
+            address paymasterId,
+            uint48 validUntil,
+            uint48 validAfter,
+            uint32 priceMarkup,
+            bytes calldata signature
+        )
+    {
+        (paymasterId, validUntil, validAfter, priceMarkup) = abi.decode(
+            paymasterAndData[VALID_PND_OFFSET:SIGNATURE_OFFSET],
+            (address, uint48, uint48, uint32)
+        );
+
+        signature = paymasterAndData[SIGNATURE_OFFSET:];
+    }
+
+    /**
+     * @dev Executes the paymaster's payment conditions
+     * @param mode tells whether the op succeeded, reverted, or if the op succeeded but cause the postOp to revert
+     * @param context payment conditions signed by the paymaster in `validatePaymasterUserOp`
+     * @param actualGasCost amount to be paid to the entry point in wei
+     */
+    function _postOp(
+        PostOpMode mode,
+        bytes calldata context,
+        uint256 actualGasCost
+    ) internal virtual override {
+        // Review what is needed in context
+        (
+            address account,
+            address paymasterId,
+            uint32 priceMarkup,
+            uint256 maxFeePerGas,
+            uint256 maxPriorityFeePerGas,
+            bytes32 userOpHash
+        ) = abi.decode(
+                context,
+                (address, address, uint32, uint256, uint256, bytes32)
+            );
+
+        uint256 effectiveGasPrice = getGasPrice(
+            maxFeePerGas,
+            maxPriorityFeePerGas
+        );
+
+        // Review max or min
+        uint32 dynamicMarkup = maxuint32(priceMarkup, fixedPriceMarkup);
+
+        uint256 balToDeduct = actualGasCost +
+            unaccountedEPGasOverhead *
+            effectiveGasPrice;
+
+        // deduct with premium
+        paymasterIdBalances[paymasterId] =
+            paymasterIdBalances[paymasterId] -
+            ((balToDeduct * dynamicMarkup) / PRICE_DENOMINATOR);
+
+        // "collect" premium
+        paymasterIdBalances[owner()] =
+            paymasterIdBalances[owner()] +
+            ((balToDeduct * (dynamicMarkup - 1e6)) / PRICE_DENOMINATOR);
+
+        emit GasBalanceDeducted(
+            paymasterId,
+            (balToDeduct * dynamicMarkup) / PRICE_DENOMINATOR
+        );
+        emit FeeCollected(
+            (balToDeduct * (dynamicMarkup - 1e6)) / PRICE_DENOMINATOR
+        );
+    }
 }
---
+++ test/sponsorship-paymaster/biconomy-verifying-paymaster-v2-specs.ts
@@ -0,0 +1,232 @@
+/* eslint-disable node/no-missing-import */
+/* eslint-disable camelcase */
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+import {
+  BiconomyAccountImplementation,
+  BiconomyAccountImplementation__factory,
+  BiconomyAccountFactory,
+  BiconomyAccountFactory__factory,
+  VerifyingSingletonPaymasterV2,
+  VerifyingSingletonPaymasterV2__factory,
+} from "../../typechain-types";
+import { fillAndSign } from "../../account-abstraction/test/UserOp";
+import { UserOperation } from "../../account-abstraction/test/UserOperation";
+import { createAccount, simulationResultCatch } from "../../account-abstraction/test/testutils";
+import { EntryPoint, EntryPoint__factory, SimpleAccount, TestToken, TestToken__factory } from "../../account-abstraction/typechain";
+
+export const AddressZero = ethers.constants.AddressZero;
+import { arrayify, hexConcat, parseEther } from "ethers/lib/utils";
+import { BigNumber, BigNumberish, Contract, Signer } from "ethers";
+
+const MOCK_VALID_UNTIL = "0x00000000deadbeef";
+const MOCK_VALID_AFTER = "0x0000000000001234";
+const dynamicMarkup = 1200000; // or 0 or 1100000
+
+export async function deployEntryPoint(
+  provider = ethers.provider
+): Promise<EntryPoint> {
+  const epf = await (await ethers.getContractFactory("EntryPoint")).deploy();
+  return EntryPoint__factory.connect(epf.address, provider.getSigner());
+}
+
+describe("EntryPoint with VerifyingPaymaster Singleton", function () {
+  let entryPoint: EntryPoint;
+  let entryPointStatic: EntryPoint;
+  let depositorSigner: Signer;
+  let walletOwner: Signer;
+  let walletAddress: string, paymasterAddress: string;
+  let ethersSigner;
+
+  let offchainSigner: Signer, deployer: Signer;
+
+  let verifyingSingletonPaymaster: VerifyingSingletonPaymasterV2;
+  let smartWalletImp: BiconomyAccountImplementation;
+  let walletFactory: BiconomyAccountFactory;
+  const abi = ethers.utils.defaultAbiCoder;
+
+  beforeEach(async function () {
+    ethersSigner = await ethers.getSigners();
+    entryPoint = await deployEntryPoint();
+    entryPointStatic = entryPoint.connect(AddressZero);
+
+    deployer = ethersSigner[0];
+    offchainSigner = ethersSigner[1];
+    depositorSigner = ethersSigner[2];
+    walletOwner = deployer; // ethersSigner[3];
+
+    const offchainSignerAddress = await offchainSigner.getAddress();
+    const walletOwnerAddress = await walletOwner.getAddress();
+
+    verifyingSingletonPaymaster =
+      await new VerifyingSingletonPaymasterV2__factory(deployer).deploy(
+        await deployer.getAddress(),
+        entryPoint.address,
+        offchainSignerAddress,
+        1100000 // fixed markup
+      );
+
+    smartWalletImp = await new BiconomyAccountImplementation__factory(deployer).deploy(
+      entryPoint.address
+    );
+
+    walletFactory = await new BiconomyAccountFactory__factory(deployer).deploy(
+      smartWalletImp.address
+    );
+
+    await walletFactory.deployCounterFactualAccount(walletOwnerAddress, 0);
+    const expected = await walletFactory.getAddressForCounterFactualAccount(
+      walletOwnerAddress,
+      0
+    );
+
+    walletAddress = expected;
+    console.log(" wallet address ", walletAddress);
+
+    paymasterAddress = verifyingSingletonPaymaster.address;
+    console.log("Paymaster address is ", paymasterAddress);
+
+    /* await verifyingSingletonPaymaster
+      .connect(deployer)
+      .addStake(0, { value: parseEther("2") });
+    console.log("paymaster staked"); */
+
+    await entryPoint.depositTo(paymasterAddress, { value: parseEther("1") });
+
+    // const resultSet = await entryPoint.getDepositInfo(paymasterAddress);
+    // console.log("deposited state ", resultSet);
+  });
+
+  async function getUserOpWithPaymasterInfo(paymasterId: string) {
+    const userOp1 = await fillAndSign(
+      {
+        sender: walletAddress,
+      },
+      walletOwner,
+      entryPoint,
+      "nonce"
+    );
+
+    const hash = await verifyingSingletonPaymaster.getHash(
+      userOp1,
+      paymasterId,
+      MOCK_VALID_UNTIL,
+      MOCK_VALID_AFTER,
+      dynamicMarkup
+    );
+    const sig = await offchainSigner.signMessage(arrayify(hash));
+    const paymasterData = abi.encode(
+      ["address", "uint48", "uint48", "uint32"],
+      [paymasterId, MOCK_VALID_UNTIL, MOCK_VALID_AFTER, dynamicMarkup]
+    );
+    const paymasterAndData = hexConcat([paymasterAddress, paymasterData, sig]);
+    return await fillAndSign(
+      {
+        ...userOp1,
+        paymasterAndData,
+      },
+      walletOwner,
+      entryPoint,
+      "nonce"
+    );
+  }
+
+  describe("#validatePaymasterUserOp", () => {
+    it("Should parse data properly", async () => {
+        const paymasterAndData = hexConcat([
+            paymasterAddress,
+            ethers.utils.defaultAbiCoder.encode(
+              ["address", "uint48", "uint48", "uint32"],
+              [
+                await offchainSigner.getAddress(),
+                MOCK_VALID_UNTIL,
+                MOCK_VALID_AFTER,
+                dynamicMarkup,
+              ]
+            ),
+            "0x" + "00".repeat(65)
+          ])
+  
+        const res = await verifyingSingletonPaymaster.parsePaymasterAndData(
+          paymasterAndData
+        );
+  
+        expect(res.paymasterId).to.equal(await offchainSigner.getAddress());
+        expect(res.validUntil).to.equal(ethers.BigNumber.from(MOCK_VALID_UNTIL));
+        expect(res.validAfter).to.equal(ethers.BigNumber.from(MOCK_VALID_AFTER));
+        expect(res.priceMarkup).to.equal(dynamicMarkup);
+        expect(res.signature).to.equal("0x" + "00".repeat(65));
+      });
+
+    it("Should Fail when there is no deposit for paymaster id", async () => {
+      const paymasterId = await depositorSigner.getAddress();
+      console.log("paymaster Id ", paymasterId);
+      const userOp = await getUserOpWithPaymasterInfo(paymasterId);
+      console.log("entrypoint ", entryPoint.address);
+      await expect(
+        entryPoint.callStatic.simulateValidation(userOp)
+        // ).to.be.revertedWith("FailedOp");
+      ).to.be.reverted;
+    });
+
+    it("succeed with valid signature", async () => {  
+      const feeCollectorBalanceBefore = await verifyingSingletonPaymaster.getBalance(await verifyingSingletonPaymaster.owner())
+      expect(feeCollectorBalanceBefore).to.be.equal(BigNumber.from(0));
+      const signer = await verifyingSingletonPaymaster.verifyingSigner();
+      const offchainSignerAddress = await offchainSigner.getAddress();
+      expect(signer).to.be.equal(offchainSignerAddress);
+
+      await verifyingSingletonPaymaster.depositFor(
+        await offchainSigner.getAddress(),
+        { value: ethers.utils.parseEther("1") }
+      );
+      const userOp1 = await fillAndSign(
+        {
+          sender: walletAddress,
+          verificationGasLimit: 200000,
+        },
+        walletOwner,
+        entryPoint,
+        "nonce"
+      );
+
+      const hash = await verifyingSingletonPaymaster.getHash(
+        userOp1,
+        await offchainSigner.getAddress(),
+        MOCK_VALID_UNTIL,
+        MOCK_VALID_AFTER,
+        dynamicMarkup
+      );
+      const sig = await offchainSigner.signMessage(arrayify(hash));
+      const userOp = await fillAndSign(
+        {
+          ...userOp1,
+          paymasterAndData: hexConcat([
+            paymasterAddress,
+            ethers.utils.defaultAbiCoder.encode(
+              ["address", "uint48", "uint48", "uint32"],
+              [
+                await offchainSigner.getAddress(),
+                MOCK_VALID_UNTIL,
+                MOCK_VALID_AFTER,
+                dynamicMarkup,
+              ]
+            ),
+            sig
+          ]),
+        },
+        walletOwner,
+        entryPoint,
+        "nonce"
+      );
+      await entryPoint.handleOps([userOp], await offchainSigner.getAddress());
+      await expect(
+        entryPoint.handleOps([userOp], await offchainSigner.getAddress())
+      ).to.be.reverted;
+
+      const feeCollectorBalanceAfter = await verifyingSingletonPaymaster.getBalance(await verifyingSingletonPaymaster.owner())
+      expect(feeCollectorBalanceAfter).to.be.greaterThan(BigNumber.from(0));
+    });
+  });
+});
