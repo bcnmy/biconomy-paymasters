004b2dc27ddcf2059395dd9e9f60e7291ca5be19
Ankur Dubey <iam@ankurdubey.com>
2023-11-02 19:18:48 +0530

    fixes for #38

--- contracts/token/adapters/UniswapExample.sol
+++ contracts/token/adapters/UniswapExample.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-pragma abicoder v2;
 
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
